e79fdefa83167e106c039b716a5ff1b3e90b991f
Alex Seres <lilaalex95@gmail.com>
2021-05-12 07:44:14 +0100

    created baseviwmodel which extends inotifypropertychanged. this viewmodel will be the parent of all viewmodel, also added propertychanged event

---
+++ WeatherForecast/ViewModels/BaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace WeatherForecast.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+    }
+}
