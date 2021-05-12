4de8299749a24e982fc5b30c3ef642627db62833
Alex Seres <lilaalex95@gmail.com>
2021-05-12 17:30:32 +0100

    finally I could orginaze horizontally orientation

--- WeatherForecast/ViewModels/MainViewModel.cs
+++ WeatherForecast/ViewModels/MainViewModel.cs
@@ -35,15 +35,34 @@ namespace WeatherForecast.ViewModels
             };
             Dicti = new Dictionary<string, decimal>()
             {
-                {"Monday", 350 },
-                {"Tuesday", 390 },
-                {"Wednesday", 400 },
-                {"Thursday", 360 },
-                {"Friday", 450 },
-                {"Saturday", 500 },
-                {"Sunday",480  }
+                {"Monday", 35 },
+                {"Tuesday", 39 },
+                {"Wednesday", 40 },
+                {"Thursday", 36 },
+                {"Friday", 45 },
+                {"Saturday", 50 },
+                {"Sunday",48  }
             };
 
         }
     }
 }
+/*
+        < ItemsControl ItemsSource = "{Binding Dicti}" >
+ 
+             < ItemsControl.ItemTemplate >
+ 
+                 < DataTemplate >
+ 
+                     < Canvas Background = "LightCyan" >
+  
+                          < Rectangle Canvas.Left = "525" Canvas.Top = "225" Height = "200" Width = "42" Fill = "Green" Panel.ZIndex = "3" />
+             
+                                 </ Canvas >
+             
+                             </ DataTemplate >
+             
+                         </ ItemsControl.ItemTemplate >
+             
+                     </ ItemsControl >
+        */
