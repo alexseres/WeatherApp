bc70dcbe1943ec85130726e2b54d74ee82479b19
Alex Seres <lilaalex95@gmail.com>
2021-05-12 07:51:53 +0100

    implemented setproperty, which will be invoked in child class

--- WeatherForecast/ViewModels/BaseViewModel.cs
+++ WeatherForecast/ViewModels/BaseViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace WeatherForecast.ViewModels
@@ -9,6 +10,14 @@ namespace WeatherForecast.ViewModels
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
+        //sets new value to the property if it has changed, invoked in child class
+        protected virtual bool SetProperty<T>(ref T stored, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if(EqualityComparer<T>.Default.Equals(stored, newValue)) return false;
 
+            stored = newValue;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
     }
 }
