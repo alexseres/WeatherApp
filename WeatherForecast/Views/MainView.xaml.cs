82d258c11ef27c97f34210d3b10672a7ff658160
Alex Seres <lilaalex95@gmail.com>
2021-05-12 09:02:24 +0100

    added mainview, and set this view as startupuri in app.xaml

--- WeatherForecast/Views/MainView.xaml.cs
+++ WeatherForecast/Views/MainView.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -13,14 +11,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WeatherForecast
+namespace WeatherForecast.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainView : Page
     {
-        public MainWindow()
+        public MainView()
         {
             InitializeComponent();
         }
