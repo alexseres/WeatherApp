22e04d8b1cd08777e5dc32be1e19e4369e18a3b4
Alex Seres <lilaalex95@gmail.com>
2021-05-12 08:13:04 +0100

    added two private fields, these methods will be delegates as arguments in viewmodels

--- WeatherForecast/ViewModels/RelayCommand.cs
+++ WeatherForecast/ViewModels/RelayCommand.cs
@@ -5,8 +5,12 @@ using System.Windows.Input;
 
 namespace WeatherForecast.ViewModels
 {
+    //the class purpose is to relay its functionality to other viewmodels by invoking this delegates
+    //so viewmoels will have the functionality to create commands by inheriting this class
     public class RelayCommand : ICommand
     {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
         public event EventHandler CanExecuteChanged;
 
         public bool CanExecute(object parameter)
