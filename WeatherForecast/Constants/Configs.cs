e80aadf627b92737965b1b6cf45ffe6f9870057f
Alex Seres <lilaalex95@gmail.com>
2021-05-13 13:27:43 +0100

    created constant fodler, and defined url and apikey in it

---
+++ WeatherForecast/Constants/Configs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherForecast.Constants
+{
+    public class Configs
+    {
+        public const string ApiKey = "e34c777bb1b5f32880f63683d74ad86d";
+        public const string CityUrl = "https://api.openweathermap.org/data/2.5/weather";
+    }
+}
--- WeatherForecast/Services/HttpClientManager.cs
+++ WeatherForecast/Services/HttpClientManager.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 
 namespace WeatherForecast.Services
 {
     public class HttpClientManager
     {
+        private HttpClient _client;
+        public HttpClient Client { get { return _client; } set { _client = value; } }
+        public HttpClientManager(HttpClient client)
+        {
+            Client = client;
+        }
     }
 }
