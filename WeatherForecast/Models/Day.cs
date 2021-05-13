7dec26264cdb856d364878a0c86b31d7a02aaec7
Alex Seres <lilaalex95@gmail.com>
2021-05-13 22:03:46 +0100

    created dto covnerter to city, right now i cannot explain properly because I have worked around 12 hours, sorry

---
+++ WeatherForecast/Models/Day.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherForecast.Models
+{
+    public class Day
+    {
+        public DateTime ExactDay { get; set; }
+
+        public decimal Temperature { get; set; }
+    }
+}
--- WeatherForecast/Services/CityService.cs
+++ WeatherForecast/Services/CityService.cs
@@ -15,20 +15,43 @@ namespace WeatherForecast.Services
             ClientManager = manager;
         }
 
-        public async Task<City> GetCity(string cityName)
+        public async Task<CityDTO> GetCity(string cityName)
         {
-            var city = await ClientManager.RequestForItem<City>(cityName);
-            var converted = DayConverter.EpochToDate(city.Kelvin.Kelvin);
+            return await ClientManager.RequestForItem<CityDTO>(cityName);
+        }
+
+        public async Task<ForecastDaysDTO> GetNextDays(float lat, float lon)
+        {
+            return await ClientManager.GetNextDayWeathers<ForecastDaysDTO>(lat, lon);
+        }
+
+
+        public async City CreateObject(string cityName)
+        {
+            var cityDTO = await GetCity(cityName);
+            ForecastDaysDTO daysDTO = await GetNextDays(cityDTO.Coordinates.Latitude, cityDTO.Coordinates.Longitude);
+            City city = new City
+            {
+                Name = cityDTO.Name,
+                ID = cityDTO.ID,
+                Temperature = KelvinConverter.ConvertKelvinToCelsius(cityDTO.Temperature.Temperature),
+                Weather = cityDTO.Weathers[0].ToString()
+            };
+            List<Day> days = new List<Day>();
+            foreach(var dayDTO in daysDTO.Days)
+            {
+                Day day = new Day()
+                {
+                    ExactDay = DayConverter.EpochToDate(dayDTO.ExactDay),
+                    Temperature = dayDTO.Temperature.Temperature
+                };
+                days.Add(day);
+            }
+            city.Days = days;
             return city;
+
+
         }
 
-        public async Task<ForecastDays> GetNextDays(float lat, float lon)
-        {
-            return await ClientManager.GetNextDayWeathers<ForecastDays>(lat, lon);
-        }
-
-        
-
-        
     }
 }
